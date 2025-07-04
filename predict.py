f281134df185c966a40a895ba90bf1b285e8a0a3
SultanMtm <149297979+SultanMtm@users.noreply.github.com>
2025-07-04 15:19:21 +0700

    Update predict.py

--- predict.py
+++ predict.py
@@ -6,7 +6,7 @@ import numpy as np
 from PIL import Image
 
 # Load model
-model = load_model("alatmusiktradtf214.h5")
+model = load_model("alatmusiktradtf214.keras")
 
 # Kelas alat musik tradisional dan deskripsinya
 class_info = {
